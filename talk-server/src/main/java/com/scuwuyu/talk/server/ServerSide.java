36a29767d69d5497001fd6659e46bee6c0105acf
吴宇 <wuyu@xkeshi.com>
2018-01-24 22:03:16 +0800

    add server side code

--- talk-server/src/main/java/com/scuwuyu/talk/server/ServerSide.java
+++ talk-server/src/main/java/com/scuwuyu/talk/server/ServerSide.java
@@ -1,5 +1,10 @@
 package com.scuwuyu.talk.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * 聊天服务端
  * Created by wuyu on 2018/1/24.
@@ -7,6 +12,12 @@ package com.scuwuyu.talk.server;
 public class ServerSide {
 
     public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
 
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        bootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
+                .childHandler(null);
     }
 }
