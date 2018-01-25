8e20f9e56353b867a2013e61fc13719f33cc166f
吴宇 <wuyu@xkeshi.com>
2018-01-25 21:03:06 +0800

    map test

---
+++ talk-server/src/main/java/com/scuwuyu/talk/test/MapTest.java
@@ -0,0 +1,14 @@
+package com.scuwuyu.talk.test;
+
+import java.util.Map;
+
+/**
+ * Created by 吴宇 on 2018-01-25.
+ */
+public class MapTest {
+
+    public static void main(String[] args) {
+//        Map<String,Object> map = {"name":"wuyu","age":12}
+        ;
+    }
+}
