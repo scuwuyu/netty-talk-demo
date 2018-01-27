177ff1625aab096aa7cdec7ab237bff52141d1b3
吴宇 <wuyu@xkeshi.com>
2018-01-27 17:07:15 +0800

    add cglib proxy

---
+++ talk-server/src/main/java/com/scuwuyu/talk/test/aop/CGLibProxy.java
@@ -0,0 +1,7 @@
+package com.scuwuyu.talk.test.aop;
+
+/**
+ * Created by 吴宇 on 2018-01-27.
+ */
+public class CGLibProxy {
+}
--- talk-server/src/main/java/com/scuwuyu/talk/test/service/impl/UserServiceImpl.java
+++ talk-server/src/main/java/com/scuwuyu/talk/test/service/impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
-package com.scuwuyu.talk.test.service;
+package com.scuwuyu.talk.test.service.impl;
 
 import com.scuwuyu.talk.test.User;
+import com.scuwuyu.talk.test.service.UserService;
 
 /**
  * Created by 吴宇 on 2018-01-27.
  */
-public class UserServerImpl implements UserService {
+public class UserServiceImpl implements UserService {
     public void insert(User user) {
         System.out.println("save success");
     }
